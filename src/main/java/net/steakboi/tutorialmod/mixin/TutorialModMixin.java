24518bc1d08f8fd0b57d1919599cb831d5dd3ae0
Timo <feldman.timo@gmail.com>
2024-10-09 23:42:11 -0500

    Added mixins

---
+++ src/main/java/net/steakboi/tutorialmod/mixin/TutorialModMixin.java
@@ -0,0 +1,19 @@
+package net.steakboi.tutorialmod.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class TutorialModMixin {
+    @Inject(method = "getJumpPower(F)F", at = @At("RETURN"), cancellable = true)
+    protected void MultiplyJumpPower(CallbackInfoReturnable<Float> power) {
+        if ((LivingEntity)(Object)this instanceof Player){
+            power.setReturnValue(power.getReturnValueF()*3);
+        }
+    }
+}
